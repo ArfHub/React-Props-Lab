c8b1a2e1a76e35ce1c621a58d445a59779b6d3c9
ARFHSI <Abdulraouf.hsi@gmail.com>
2024-06-09 19:14:30 +0300

    W7-D1-Lab1

---
+++ props-lab/src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Company from './component/Company'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+<Company CompanyName = "Aramco"/>
+<Company WorkerNumber = "10293"/>
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ props-lab/src/component/Company.jsx
@@ -0,0 +1,14 @@
+function Company(props){
+    return (
+
+<div className='bgc'>
+<h1>CompanyName:{props.CompanyName}</h1>
+<h1>Worker Number:{props.WorkerNumber}</h1>
+
+
+
+</div>
+      )
+}
+
+export default Company
